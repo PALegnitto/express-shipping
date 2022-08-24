357cab872b65dfd4b999beda7da94aa1f2d163e9
Phill Legnitto <palegnitto@gmail.com>
2022-08-23 17:50:38 -0700

    resolved requirement bug

--- routes/shipments.js
+++ routes/shipments.js
@@ -4,9 +4,9 @@ const express = require("express");
 const router = new express.Router();
 
 const jsonschema = require("jsonschema");
-const orderSchema = (".../schemas/orderSchema.json");
+const orderSchema = require("../schema/orderSchema.json");
 
-const {BadRequestError} = require("../expressError")
+const { BadRequestError } = require("../expressError");
 
 const { shipProduct } = require("../shipItApi");
 
@@ -21,14 +21,17 @@ const { shipProduct } = require("../shipItApi");
 router.post("/", async function (req, res, next) {
   // const { productId, name, addr, zip } = req.body;
   // const shipId = await shipProduct({ productId, name, addr, zip });
-
-  const result = jsonschema.validate(req.body, orderSchema, {required:true});
-
-  if (result.valid){
+  console.log('req.body', req.body);
+  const result = jsonschema.validate(req.body, orderSchema, { required: true });
+  console.log(result);
+  
+  if (result.valid) {
     const shipId = await shipProduct(req.body);
+    console.log(shipId);
+
     return res.json({ shipped: shipId });
   } else {
-    const errs= result.errors.map(err => err.stack);
+    const errs = result.errors.map(err => err.stack);
     throw new BadRequestError(errs);
   }
 });
--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -3,7 +3,7 @@
 const request = require("supertest");
 const app = require("../app");
 
-
+/** Tests that  */
 describe("POST /", function () {
   test("valid", async function () {
     const resp = await request(app).post("/shipments").send({
@@ -14,5 +14,19 @@ describe("POST /", function () {
     });
 
     expect(resp.body).toEqual({ shipped: expect.any(Number) });
+
   });
+  
+    test("invalid", async function () {
+      const resp = await request(app).post("/shipments").send({
+        productId: 1000,
+        name: "Test Tester",
+        zip: "12345-6789",
+      });
+
+      expect(resp.body.error.message).toEqual(["instance requires property \"addr\""]);
+      expect(resp.statusCode).toEqual(400);
+    });
+
 });
+
